29c88849d8f13b015ac5dc221353086842e24d96
Emre Çanakçı <emreosmancanakci@gmail.com>
2025-03-19 22:48:11 +0300

    Kosullar (if-else) konusu tamamlandı

---
+++ src/ConditionsDemo.java
@@ -0,0 +1,17 @@
+
+public class ConditionsDemo {
+
+	public static void main(String[] args) {
+		int number = 10;
+
+        if (number > 0) {
+            System.out.println("Sayı pozitiftir.");
+        } else if (number < 0) {
+            System.out.println("Sayı negatiftir.");
+        } else {
+            System.out.println("Sayı sıfırdır.");
+        }
+
+	}
+
+}
