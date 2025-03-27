36c72ba7c011ec655069e38bff7ca4e5acba8d7f
emreosminho <emreosmancanakci@gmail.com>
2025-03-27 22:19:03 +0300

    Temal Operatörler - Mantıksal

--- src/TemelOperatorler.java
+++ src/TemelOperatorler.java
@@ -1,2 +1,27 @@
 public class TemelOperatorler {
+    public static void main(String[] args) {
+        /*
+        // = atama operatörü
+        int a = 5;
+        int b = a;
+        System.out.println(b);
+
+        // += sagındaki değeri solundaki değerle toplayıp tekrar solundaki değere atar.
+        int d = 7, e = 4;
+        d += e;
+        System.out.println(d);
+         */
+
+        // Karşılaştırma operatörleri
+        int a = 5, b = 6, c = 10;
+        boolean sonuc = (a != b);
+        boolean sonuc1 = (c>a);
+        System.out.println(sonuc);
+        System.out.println(sonuc1);
+
+        // Mantıksal Operatörler || veya - && ve
+
+
+
+    }
 }
