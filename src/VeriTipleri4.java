f129249194fb4b467d349b24ad26f992896495a1
emreosminho <emreosmancanakci@gmail.com>
2025-03-27 22:04:30 +0300

    VeriTipleri4 char and boolean

---
+++ src/VeriTipleri4.java
@@ -0,0 +1,41 @@
+public class VeriTipleri4 {
+    public static void main(String[] args) {
+
+        char vChar = 'b';
+        char v2Char = 98;
+        System.out.println(vChar);
+        System.out.println(v2Char);
+
+        char c1 = 'J';
+        char c2 = 'A';
+        char c3 = 'V';
+        char c4 = 'A';
+
+        System.out.println(" " + c1+c2+c3+c4);
+
+
+        // Boolean
+        boolean dogru = true;
+        boolean yanlis = false;
+        System.out.println(dogru);
+
+        /* Char
+        Java'da karakter değişkenleri saklamak için Char kullanılır. Char veri tipleri birleşerek String Sınıfından bir yapıya dönüşür.
+
+        Karakterler Char ile saklanır.
+        Diğer dillere göre Char Java’da 16 bittir.
+                Java Unicode karakter setini kullanır ve tüm dilleri içerir.
+        Java evrensel bir dil olarak tasarlandığı için karakter seti de evrensel set olan Unicode ile tanımlanmıştır.
+        Anahtar Sözcük : char
+                Boolean
+        Java, mantıksal değerleri saklamak için boolean adında bir tipe sahiptir.
+
+                Boolean sadece iki değer alabilir : True ve False
+        Genellikle koşul ve döngü işlemlerinde, kontrol amaçlı olarak kullanılır.
+                Anahtar Sözcük : boolean */
+
+
+
+    }
+}
+
