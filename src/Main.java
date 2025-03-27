b885bcd18f2e6ac7acb7d7b6b1400016075b29f4
emreosminho <emreosmancanakci@gmail.com>
2025-03-27 16:47:17 +0300

    Temel Kavramlar Degiskenler

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //
+        int numberOne, numberTwo = 2, numberThree;
+        numberOne = 1;
+        numberThree = numberOne + numberTwo;
+        System.out.println(numberOne);
+        System.out.println(numberTwo);
+        System.out.println(numberThree);
+    }
+}
