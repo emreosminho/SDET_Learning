dc1c2d8cf58a24ca9a78545eaf6831dbaba83885
emreosminho <emreosmancanakci@gmail.com>
2025-03-27 17:01:56 +0300

    Veri Tipleri Devam

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        //
+        //Temel Kavramlar ve Değişkenler
         int numberOne, numberTwo = 2, numberThree;
         numberOne = 1;
         numberThree = numberOne + numberTwo;
         System.out.println(numberOne);
         System.out.println(numberTwo);
         System.out.println(numberThree);
+
+        // Kare çevre ve alan hesaplama
+        int kisaKenar = 20;
+        int uzunKenar = 40;
+        int alan = kisaKenar * uzunKenar;
+        int cevre = 2*kisaKenar + 2*uzunKenar;
+
+        System.out.println("Kare Alanı: " + alan);
+        System.out.println("Kare Çevresi: " + cevre);
+
+        // Tam Sayılar -> Byte, Short, Integer, Lon,
+        // Ondalık Sayılıar -> Float, Double
+        // Karakter -> Char
+        // Mantıksal Degerler -> Boolean
+
     }
 }
