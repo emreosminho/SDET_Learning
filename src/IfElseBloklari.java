b235fa00a992ac742f2e62971fef8b46c1921abd
emreosminho <emreosmancanakci@gmail.com>
2025-03-28 11:48:20 +0300

    If Else blokları

---
+++ src/IfElseBloklari.java
@@ -0,0 +1,12 @@
+public class IfElseBloklari {
+    public static void main(String[] args){
+       int a = 10, b = 20;
+       if (a == b) {
+           System.out.println("a eşittir b'ye");
+       } else if (a < b) {
+           System.out.println("a kucuktur b'den");
+       } else {
+           System.out.println("a buyuktur b'den");
+       }
+    }
+}
