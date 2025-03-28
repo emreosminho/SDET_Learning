202a3f15d037a17d7b15f47e20bba39acae7f832
emreosminho <emreosmancanakci@gmail.com>
2025-03-28 11:42:41 +0300

    Kod Blokları

---
+++ src/KodBloklari.java
@@ -0,0 +1,10 @@
+public class KodBloklari {
+    // sınıfa ait alan
+    int a = 10;
+    public static void main(String[] args){
+        // main metoduna ait alan
+        System.out.println("Burasi main sınıfına aittir.");
+        int a = 5;
+        System.out.println(a);
+    }
+}
