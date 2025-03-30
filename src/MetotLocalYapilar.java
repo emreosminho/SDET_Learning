b5b9165027a13cb42e03414a8b26005080d6a4f8
emreosminho <emreosmancanakci@gmail.com>
2025-03-30 11:52:37 +0300

    Local Degiskenler

---
+++ src/MetotLocalYapilar.java
@@ -0,0 +1,11 @@
+public class MetotLocalYapilar {
+    static void sum(int a, int b){
+        // a = 25;
+        System.out.println(a);
+    }
+    public static void main(String[] args){
+       int a = 20;
+       int b = 10;
+       sum(a,b);
+    }
+}
