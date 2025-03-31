245e28e42505ffe2d1bba08f88f4e4d566d8c934
emreosminho <emreosmancanakci@gmail.com>
2025-03-31 19:02:21 +0300

    Polidrom Sayi

---
+++ src/MetotPalindromSayi.java
@@ -0,0 +1,20 @@
+public class MetotPalindromSayi {
+
+    static boolean isPolidrom(int number){
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0){
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+            System.out.println(reverseNumber);
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args){
+        isPolidrom(247);
+
+
+    }
+}
