476ed07aba7ca9eacf6cdf3b683fce95090a836a
emreosminho <emreosmancanakci@gmail.com>
2025-04-02 11:32:36 +0300

    Constructor Yapıcı Metotlar

--- src/Car.java
+++ src/Car.java
@@ -3,16 +3,32 @@ class Car {
     String model;
     String color;
     int speed;
-    int speedLimit = 180;
+    int speedLimit;
+
+
+    Car(String model, int speed, String color) {
+        this.model = model;
+        this.speed = speed;
+        this.color = color;
+        this.type = "Sedan";
+        this.speedLimit = 180;
+    }
 
     void hizlanma(int hizHesap){
         if ((speed + hizHesap) < speedLimit){
-            speed += hizHesap;
+            this.speed += hizHesap;
         }
     }
 
     void printSpeed(){
         System.out.println(model + " Hızı: " + speed);
+    }
+
+    void printInfo(){
+        System.out.println("Model : " + this.model);
+        System.out.println("Color : " + this.color);
+        System.out.println("Type : " + this.type);
+        System.out.println("Speed : " + this.speed);
     }
 
 }
--- src/CarMain.java
+++ src/CarMain.java
@@ -1,15 +1,15 @@
 public class CarMain {
     public static void main(String[] args){
-        Car audi = new Car();
-        audi.model = "Audi A3";
-        audi.speed = 10;
-        audi.hizlanma(50);
-        audi.hizlanma(60);
-        audi.hizlanma(59);
-        audi.printSpeed();
+        Car audi = new Car("Audi", 179, "Siyah");
+        audi.printInfo();
+
+        System.out.println("=====================");
+
+        Car bmw = new Car("BWM", 180,"Mavi");
+        bmw.printInfo();
 
 
-        Car bmw = new Car();
+        /* Car bmw = new Car();
         bmw.model = "BMW";
         bmw.speed = 20;
         bmw.printSpeed();
@@ -18,7 +18,7 @@ public class CarMain {
         Car mercedes = new Car();
         mercedes.model = "Mercedes";
         mercedes.speed = 30;
-        mercedes.printSpeed();
+        mercedes.printSpeed(); */
 
 
 
