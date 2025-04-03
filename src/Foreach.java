8484876cb155bcd02ccfe8d2c3c9949a1f82553f
emreosminho <emreosmancanakci@gmail.com>
2025-04-03 10:20:40 +0300

    Foreach matris

--- src/Foreach.java
+++ src/Foreach.java
@@ -16,9 +16,22 @@ public class Foreach {
         }
         System.out.println("Dizi elemanları toplamaı: " + sum); */
 
-        String[] araclar = {"Audi", "Bmw", "Tofas"};
+       /* String[] araclar = {"Audi", "Bmw", "Tofas"};
         for (String str: araclar){
             System.out.println(str);
+        } */
+
+        int[][] matris = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        for (int i = 0; i < matris.length ; i++){
+            for(int j = 0; j<matris[i].length; j++){
+                System.out.print(matris[i][j]);
+            }
+            System.out.println();
         }
 
     }
