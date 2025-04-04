57289a33a858307fad1ffbe29808ad0252fc0ab8
emreosminho <emreosmancanakci@gmail.com>
2025-04-04 12:14:44 +0300

    Erisim belirleyiciler 1

---
+++ src/first/First.java
@@ -0,0 +1,13 @@
+package first;
+
+public class First {
+    private String str;
+
+    First(String str){
+        this.str = str;
+    }
+
+    void ShowStr(){
+        System.out.println(this.str);
+    }
+}
